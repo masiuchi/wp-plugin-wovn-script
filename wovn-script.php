0daf800a1eae6222555acfc6da7100eb14373365
Masahiro Iuchi <masahiro.iuchi@gmail.com>
2016-08-11 03:41:21 +0000

    Version 0.01.

---
+++ wovn-script.php
@@ -0,0 +1,62 @@
+<?php
+/*
+
+Plugin Name: WOVN Script
+Plugin URI:  https://github.com/masiuchi/wp-plugin-wovn-script
+Description: Insert WOVN.io script tag to HTML.
+Version:     0.01
+Author:      Masahiro Iuchi
+Author URI:  https://github.com/masiuchi
+License:     GPL2
+
+Copyright 2016 Masahiro Iuchi (email : masahiro.iuchi@gmail.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+
+add_action('wp_head', function () {
+  $token = esc_attr(get_option('wovn_script_token'));
+  if ($token) {
+    echo "<script src=\"//j.wovn.io/1\" data-wovnio=\"key=$token\" async></script>\n";
+  }
+});
+
+if (is_admin()) {
+  add_action('admin_init', function () {
+    register_setting('wovn-script', 'wovn_script_token');
+  });
+
+  add_action('admin_menu', function () {
+    add_options_page( 'WOVN Script Settings', 'WOVN Script', 'manage_options', 'wovn-script', function () {
+      echo '<div class="wrap">';
+      echo '  <h1>WOVN Script setting</h1>';
+      echo '  <form method="post" action="options.php">';
+      echo      settings_fields('wovn-script');
+                do_settings_sections('wovn-script');
+      echo '    <table class="form-table">';
+      echo '      <tr>';
+      echo '        <th scope="row"><label for="wovn-script-token">Token</label></th>';
+      echo '        <td><input id="wovn-script-token" type="text" name="wovn_script_token" value="' . get_option('wovn_script_token') . '" /></td>';
+      echo '      </tr>';
+      echo '    </table>';
+      echo      submit_button();
+      echo '  </form>';
+      echo '</div>';
+    });
+  });
+}
+
+?>
+
